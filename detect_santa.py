58ce5040461061d4ce3910b80c7f9913b1f91b90
micah masai kipkirui <103409704+Micah1999Masai@users.noreply.github.com>
2022-12-11 20:19:46 +0300

    Update detect_santa.py

--- detect_santa.py
+++ detect_santa.py
@@ -2,13 +2,15 @@ from PIL import Image
 import numpy as np
 import sys
 
-filename = sys.argv[1]
-image = Image.open(filename).convert('RGB')
-im_width, im_height = 150, 150
-image = image.resize((im_width, im_height), Image.ANTIALIAS)
-image_np = np.array(image.getdata()).reshape(
-    (1, im_height, im_width, 3)).astype(np.float32) / 255.
-
+def main() :
+    file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
+    if file_uploaded is not None :
+        image = Image.open(file_uploaded)
+        st.write("Uploaded Image.")
+        figure = plt.figure()
+        plt.imshow(image)
+        plt.axis('off')
+        st.pyplot(figure)
 model = load_model('mynetwork.h5')
 pred = model.predict(image_np)
 print(pred)
